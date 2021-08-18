2f3c3bce3f33136fd40eb1e8c60e13886b95b653
NamanGoyal07 <naman18@iitg.ac.in>
2021-08-18 16:13:13 +0530

    Shyd Cors error solved

--- popup.js
+++ popup.js
@@ -12,8 +12,25 @@ let recentContestLink = document.getElementById("recentContestLink");
 let handle_selected = document.getElementById("handle_selected");
 let handle_options = document.getElementById("handle_options");
 let show_handle = document.getElementById("show_handle");
-let handle="HighVoltage";
+let handle="NamanGoyal07";
 show_handle.innerHTML = "Handle selected: <b>" + handle + "</b>";
+
+document.addEventListener("DOMContentLoaded",function() {
+    var cors_api_host = 'cors-anywhere.herokuapp.com';
+    var cors_api_url = 'https://' + cors_api_host + '/';
+    var slice = [].slice;
+    var origin = window.location.protocol + '//' + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function() {
+        var args = slice.call(arguments);
+        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
+            targetOrigin[1] !== cors_api_host) {
+            args[1] = cors_api_url + args[1];
+        }
+        return open.apply(this, args);
+    };
+});
 
 chrome.storage.sync.get(
     ["darkMode"],
